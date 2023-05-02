2fb9b06afa592e9dc256d7847351d6499aa546f7
mabualheaja <malak.heaja@gmail.com>
2023-05-02 21:14:40 +0300

    filterableProductTable with SearchBar

--- labo/src/App.jsx
+++ labo/src/App.jsx
@@ -6,18 +6,42 @@ import './App.css'
 
 
 const PRODUCTS = [
-  {id:1, title:""},
+  {category:"Fruits", price:"$5", stocked: true, name: "Apple"},
+  {category:"Fruits", price:"$3", stocked: true, name: "Orange"},
+  {category:"Fruits", price:"$8", stocked: false, name: "Abrichot"},
+
+  {category:"Vegetables", price:"$7", stocked: true, name: "Potato"},
+  {category:"Vegetables", price:"$10", stocked: false, name: "Spinach"},
+  {category:"Vegetables", price:"$14", stocked: true, name: "Pumpkin"},
 ];
 
 const FilterableProductTable = () => {
+  return (
+  <div>
+     <SearchBar/>
+  </div>
+  );
+};
 
-}
 const SearchBar = () => {
-
+  return (
+    <form>
+      <input style={{display: "block"}} type="text" placeholder="Search ..."/>
+      <label>
+        <input type="checkbox"/>
+        {''} Only show products in stock
+      </label>
+    </form>
+  );
 }
 
 const ProductTable = () => {
-
+  return (
+    <div>
+      <ProductCategoryRow/>
+      <ProductRow/>
+    </div>
+  )
 }
 
 const ProductCategoryRow = () => {
@@ -30,8 +54,6 @@ const ProductRow = () => {
 
 function App() {
   const [count, setCount] = useState(0);
-  const [primaryColor, setPrimaryColor] = useState("pink");
-  const [secondaryColor, setSecondaryColor] = useState("brown");
   return (
     <FilterableProductTable/>
   )
